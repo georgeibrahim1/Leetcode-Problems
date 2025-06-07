a741590a3fd6bbcc7d99ceae93bec6aec1cf3c0b
George Ibrahim <103423847+georgeibrahim1@users.noreply.github.com>
2025-06-08 02:56:37 +0300

    [111] [Time Beats: 100.00%] [Memory Beats: 89.51%] - LeetPush

---
+++ Minimum-Depth-of-Binary-Tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(!root) return 0;
+        if(root && !root->left && !root->right) return 1;
+
+        return rec_min(root);
+    }
+
+    int rec_min(TreeNode* node)
+    {
+        if(!node) return 0;
+
+        int left_depth = rec_min(node->left);
+        int right_depth = rec_min(node->right);
+
+        if(!left_depth && right_depth)
+        {
+            return right_depth + 1;
+        }
+
+        if(left_depth && !right_depth)
+        {
+            return left_depth + 1;
+        }
+
+        if(left_depth <= right_depth)
+            return left_depth + 1;
+        else
+            return right_depth + 1;
+    }
+};
