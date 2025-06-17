a2df26f90c379e1db951d5ac3e88068e3ec8c728
George Ibrahim <103423847+georgeibrahim1@users.noreply.github.com>
2025-06-18 00:06:17 +0300

    [501] [Time Beats: 58.24%] [Memory Beats: 39.89%] - LeetPush

--- Find-Mode-in-Binary-Search-Tree.cpp
+++ Find-Mode-in-Binary-Search-Tree.cpp
@@ -9,31 +9,65 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     map<int,int> m;
+//     int max_freq = 0;
+//     vector<int> findMode(TreeNode* root) {
+//         vector<int> aaaaaaaaaaaa7;
+//         rec_ok(root);
+//         for(auto it : m) {
+//             if(it.second == max_freq) aaaaaaaaaaaa7.push_back(it.first);
+//         }
+//         return aaaaaaaaaaaa7;
+//     }
+
+//     void rec_ok(TreeNode* node)
+//     {
+//         if(!node) return;
+//         if(m.find(node->val) == m.end()) {
+//             m[node->val] = 1;
+//         }
+//         else {
+//             m[node->val]++;
+//         }
+//         if(m[node->val] > max_freq) max_freq = m[node->val];
+//         rec_ok(node->left);
+//         rec_ok(node->right);
+
+//     }
+// };
 class Solution {
 public:
-    map<int,int> m;
+    unordered_map<int,int> m;
     int max_freq = 0;
+    vector<int> aaaaaaaaaaaa7;
     vector<int> findMode(TreeNode* root) {
-        vector<int> aaaaaaaaaaaa7;
         rec_ok(root);
-        for(auto it : m) {
-            if(it.second == max_freq) aaaaaaaaaaaa7.push_back(it.first);
-        }
         return aaaaaaaaaaaa7;
     }
 
     void rec_ok(TreeNode* node)
     {
         if(!node) return;
-        if(m.find(node->val) == m.end()) {
-            m[node->val] = 1;
-        }
-        else {
-            m[node->val]++;
-        }
-        if(m[node->val] > max_freq) max_freq = m[node->val];
         rec_ok(node->left);
-        rec_ok(node->right);
+        int count = ++m[node->val]; //this eplaces all my fucking lines
+        // if(m.find(node->val) == m.end()) {
+        //     m[node->val] = 1;
+        // }
+        // else {
+        //     m[node->val]++;
+        // }
 
+        //this comes form the inorder proberity with BSTs
+        if(count > max_freq) {
+            max_freq = count;
+            aaaaaaaaaaaa7.clear();
+            aaaaaaaaaaaa7.push_back(node->val);
+        }
+        else if(count == max_freq) {
+            aaaaaaaaaaaa7.push_back(node->val);
+        }   
+        rec_ok(node->right);
     }
 };
