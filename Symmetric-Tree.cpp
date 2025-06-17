e37d70e2ea22888621a33b53a63c175abb5e0449
George Ibrahim <103423847+georgeibrahim1@users.noreply.github.com>
2025-06-17 18:56:21 +0300

    [101] [Time Beats: 100.00%] [Memory Beats: 6.47%] - LeetPush

---
+++ Symmetric-Tree.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) { 
+        if(root && !root->left && root->right) return false;
+        if(root && root->left && !root->right) return false;
+        if(root && root->left && root->right && !root->left->left && !root->left->right && 
+            !root->right->left && !root->right->right) return root->left->val==root->right->val;       
+        bool ans = true;
+        queue<TreeNode*> q;
+        q.push(root);
+        int last_level = 0;
+        while(!q.empty())
+        {
+            int sz = q.size();
+            vector<int> v;
+            for(int i = 0 ; i < sz ;i++)
+            {
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left)
+                {
+                    q.push(node->left);
+                    v.push_back(node->left->val);
+                }
+                else
+                {
+                    v.push_back(-101);
+                }
+
+                if(node->right)
+                {
+                    q.push(node->right);
+                    v.push_back(node->right->val);
+                }
+                else
+                {
+                    v.push_back(-101);
+                }
+            }
+            last_level++;
+            for (int i = 0; i < v.size() / 2; i++) {
+                if (v[i] != v[v.size() - 1 - i]) {
+                    return false; // early return on mismatch
+                }
+            }   
+        }
+
+        return ans;
+    }
+};
